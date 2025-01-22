79dcbe4344964b8a43da2a414f1b032ea601917d
joshlawlor <joshlawlor1@gmail.com>
2025-01-22 16:09:33 -0500

    Basic Sum Program

--- src/Basic.java
+++ src/Basic.java
@@ -1,2 +1,11 @@
+
 public class Basic {
+
+    public static void main(String[] args){
+
+        int x,y;
+        x = 10;
+        y = 15;
+        System.out.println("The sum of x and y is: "+ (x+y));
+    }
 }
