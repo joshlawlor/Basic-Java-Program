7f4df653805865dec4e723bbbee7479cc5d0a4df
joshlawlor <joshlawlor1@gmail.com>
2025-01-23 15:19:44 -0500

    Arrays Processing setup

---
+++ src/ArraysPractice.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class ArraysPractice {
+
+    public static void main(String[] args){
+
+        int[] array = new int[10];
+
+        array[1] = 56;
+
+        System.out.print(Arrays.toString(array));
+    }
+
+}
