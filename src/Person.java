2d3bbde6d94a45b900715b0c59e568df2235f607
joshlawlor <joshlawlor1@gmail.com>
2025-01-23 11:13:15 -0500

    User Defined Class practice

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+
+
+/*  Goals:
+ *  1. Set the given name.
+ *  2. Return the given name.
+ *  3. Set the surname.
+ *  4. Return the surname.
+ *  5. Set the age.
+ *  6. Return the age.
+ *  7. Increment the age.
+ *  8. Compare two persons for equality.
+ *  9. Copy a person.
+ * 10. Return a copy of a person.
+ * 11. Print a person.
+ */
+public class Person {
+    private String givenName;
+    private String surName;
+    private int age;
+
+}
+
+
