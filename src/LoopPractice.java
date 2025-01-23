693042ba8fabe4c01977a8024209449055dafa0a
joshlawlor <joshlawlor1@gmail.com>
2025-01-22 20:08:37 -0500

    Loop Practice program - added guessLimit

--- src/LoopPractice.java
+++ src/LoopPractice.java
@@ -7,25 +7,29 @@ public class LoopPractice {
 
 
     public static void main(String[] args){
-        int randomNumber, guess;
+        int randomNumber, guess, guessLimit;
         boolean found;
-
+        guessLimit = 0;
         randomNumber = (int)(Math.random() * 20 + 1);
 
-        System.out.println("I'm thinking of a number between 1 and 20.");
+        System.out.println("I'm thinking of a number between 1 and 20. You will have 3 guesses!");
 
         found = false;
 
-        while( !found){
+        while( !found && guessLimit <= 2){
             System.out.print("Guess the value between 1 and 20: ");
             guess = kb.nextInt();
             if(guess == randomNumber){
                 found = true;
             }else
                 System.out.print("It's not " + guess + ".\n");
+            guessLimit++;
         }
 
-        System.out.println("\n You got it! The answer is: " + randomNumber);
+        if(found)
+            System.out.println("\nYou got it! The answer is: " + randomNumber);
+        else
+            System.out.println("Sorry, you lost! The answer is: " + randomNumber);
 
 
 
